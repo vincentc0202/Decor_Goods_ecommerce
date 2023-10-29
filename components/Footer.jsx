191a5540d041663cac40ace4650332ce680cacea
Vincent <vcheong0202@gmail.com>
2023-10-29 02:41:18 -0700

    1st commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2023 Decor Goods</p>
+      {/* {<p>All rights reserved</p>} */}
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '50fxz52g',
+    dataset: 'production'
+  }
+})
